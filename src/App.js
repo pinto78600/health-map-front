8f75de3cfe248074593e42396231a54510fde890
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2020-06-25 11:10:24 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Map from './components/pages/Map'
+// import Map from './components/pages/Map'
 import './App.css'
+import VaccinationCountry from './components/pages/VaccinationCountry'
 
 function App() {
 
   return (
     <div className="App">
-      <Map />
+      <VaccinationCountry />
     </div>
   );
 }
